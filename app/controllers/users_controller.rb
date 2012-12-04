4233d9c7bfc28cb77294f5e5cf307461f3c521ce
Brad Graham <blg002@gmail.com>
2012-12-03 21:52:51 -0500

    finished: 9.1 Updating users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,16 @@
 class UsersController < ApplicationController
  
+  # GET /users/1
   def show
     @user = User.find(params[:id])
   end
 
+  # GET /users/new
   def new
     @user = User.new
   end
 
+  # POST /users
   def create
     @user = User.new(params[:user])
 
@@ -17,6 +20,24 @@ class UsersController < ApplicationController
       redirect_to @user
     else
       render 'new'
+    end
+  end
+
+  # GET /users/1/edit
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  # PUT /users/1
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update_attributes(params[:user])
+      sign_in @user
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
     end
   end
 end
