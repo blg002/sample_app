b6179e6ce52b0ff8c08d3e2428269bf8f383b3da
Brad Graham <blg002@gmail.com>
2012-12-03 23:33:28 -0500

    finished: 9.2 Authorization

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_filter :signed_in_user, only: [:edit, :update]
+  before_filter :correct_user,   only: [:edit, :update]
  
   # GET /users/1
   def show
@@ -25,13 +27,10 @@ class UsersController < ApplicationController
 
   # GET /users/1/edit
   def edit
-    @user = User.find(params[:id])
   end
 
   # PUT /users/1
   def update
-    @user = User.find(params[:id])
-
     if @user.update_attributes(params[:user])
       sign_in @user
       flash[:success] = "Profile updated"
@@ -40,4 +39,18 @@ class UsersController < ApplicationController
       render 'edit'
     end
   end
+
+  private
+
+    def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Please sign in." unless signed_in?
+      end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) unless current_user?(@user)
+    end
 end
