dea5ec210a4c08eff089e75d1e8c196175b32a69
Brad Graham <blg002@gmail.com>
2012-12-21 17:35:59 -0800

    finished: 9.3 Showing all users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,12 @@
 class UsersController < ApplicationController
-  before_filter :signed_in_user, only: [:edit, :update]
+  before_filter :signed_in_user, only: [:index, :edit, :update]
   before_filter :correct_user,   only: [:edit, :update]
  
+  # GET /users
+  def index
+    @users = User.paginate(page: params[:page])
+  end
+
   # GET /users/1
   def show
     @user = User.find(params[:id])
