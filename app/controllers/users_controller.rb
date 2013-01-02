f85e2a24f7ed129dfd7cfcdb7ee80e50516990cd
Brad Graham <blg002@gmail.com>
2013-01-01 18:45:02 -0800

    finished: 10.2 Showing microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
   # GET /users/1
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
 
   # GET /users/new
